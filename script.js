4b5ec34276e9bceddd260839bced6ece5b66449d
Israel Freitas Mendes <israel.mendes@ibm.com>
2024-01-07 04:29:11 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  // if(html.classList.contains("light")){
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+
+  //Pegar a tag IMG
+  const img = document.querySelector("#profile img")
+  //Substituir a imagem
+  if (html.classList.contains("light")) {
+    //Se tiver ligth mode ativar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //Se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/israel.jpeg")
+  }
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("alt", "Ligth")
+  } else {
+    img.setAttribute("alt", "Dark")
+  }
+}
